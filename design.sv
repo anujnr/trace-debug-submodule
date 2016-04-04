7318b0143f0df1df37aeaf37e8535f58b1f7222f
Anuj Rao <anr044@ucsd.edu>
2016-04-04 01:54:57 -0700

    Update design.sv formatting

--- design.sv
+++ design.sv
@@ -4,12 +4,10 @@
 //
 
 module dut #(parameter sample_width_p = 16
-            		   , counter_width_p = 16)
+             , counter_width_p = 16)
   (input clk
-   
    , input [sample_width_p-1:0]	sample_data
    , input	sample_valid
-   
    , output logic [sample_width_p:0] fifo_data
    , output logic fifo_valid
    , input fifo_ready
@@ -18,12 +16,11 @@ module dut #(parameter sample_width_p = 16
 //   assert(sample_width_p >= counter_width_p)
 //  	else $error("Sample_width must be atleast as much as the counter_width!");
 
-  //how to initialise?
   logic [counter_width_p-1:0] ctr;  
   
   always_ff @(posedge clk)
     begin
-      
+    
       if(fifo_ready && ctr==1)
         begin
           fifo_data <= sample_valid? {1'b0,sample_data}: '0;
@@ -34,18 +31,18 @@ module dut #(parameter sample_width_p = 16
       else if(fifo_ready) //non-zero ctr
         begin
           fifo_data <= {1'b1,ctr};
-        	fifo_valid <= 1'b1;
-        	ctr <= 1;
+          fifo_valid <= 1'b1;
+          ctr <= 1;
         end
       
       else if(~fifo_ready && sample_valid)
         begin
           ctr <= ctr + 1;
           fifo_data  <= fifo_data;
-         	fifo_valid <= fifo_valid;
+          fifo_valid <= fifo_valid;
         end
   
-      else
+    else
 		begin
           ctr <= ctr;
           fifo_data  <= fifo_data;
