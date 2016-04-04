ea8f4298b27c25d1922eb8e6795190ea18c559d5
Anuj Rao <anr044@ucsd.edu>
2016-04-04 14:37:03 -0700

    Update design.sv

--- design.sv
+++ design.sv
@@ -6,8 +6,11 @@
 module dut #(parameter sample_width_p = 16
              , counter_width_p = 16)
   (input clk
+   , input rst
+  
    , input [sample_width_p-1:0]	sample_data
    , input	sample_valid
+   
    , output logic [sample_width_p:0] fifo_data
    , output logic fifo_valid
    , input fifo_ready
@@ -17,38 +20,52 @@ module dut #(parameter sample_width_p = 16
 //  	else $error("Sample_width must be atleast as much as the counter_width!");
 
   logic [counter_width_p-1:0] ctr;  
+
+  always_comb
+    begin      
+
+      if(fifo_ready && ctr!=0) //ctr had been incremented 
+        begin
+          fifo_data = {1'b1,ctr};
+          fifo_valid = 1'b1;
+        end
+
+      else if(fifo_ready) //generic case
+        begin
+          fifo_data = sample_valid? {1'b0,sample_data}: '0;
+      	  fifo_valid = sample_valid;
+        end
+      
+      else if(~fifo_ready && sample_valid) //data loss
+        begin
+          fifo_data  = '0;
+          fifo_valid = '0; 
+        end
+  
+      else
+	begin
+          fifo_data  = fifo_data;
+      	  fifo_valid = fifo_valid;
+        end
+    end
   
   always_ff @(posedge clk)
     begin
-    
-      if(fifo_ready && ctr==1)
-        begin
-          fifo_data <= sample_valid? {1'b0,sample_data}: '0;
-      	  fifo_valid <= sample_valid;
-      	  ctr <= ctr;
-        end
       
-      else if(fifo_ready) //non-zero ctr
-        begin
-          fifo_data <= {1'b1,ctr};
-          fifo_valid <= 1'b1;
-          ctr <= 1;
-        end
-      
-      else if(~fifo_ready && sample_valid)
-        begin
+      //second condition is to reset the counter after error packet is sent
+      if(rst || (fifo_ready && fifo_data[sample_width_p]==1'b1))
+          ctr <= 0;      
+ 
+      //Corner case: fifo is ready asserts for only one clock cycle and the error packet was sent sent
+      else if(~fifo_ready && fifo_data[sample_width_p]==1'b1)
+          ctr <= 1;      
+
+      //second condition below factors in the error packet
+      else if((~fifo_ready && sample_valid && ~(&ctr)) || (sample_valid && fifo_ready && ctr!=0 && ~(&ctr))) 
           ctr <= ctr + 1;
-          fifo_data  <= fifo_data;
-          fifo_valid <= fifo_valid;
-        end
-  
-    else
-		begin
-          ctr <= ctr;
-          fifo_data  <= fifo_data;
-      	  fifo_valid <= fifo_valid;
-        end
       
+      else
+          ctr <= ctr;      
     end
       
 endmodule
