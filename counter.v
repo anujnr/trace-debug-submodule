7d9040b418d5aa135a7dbad3e15762dfb7c35cca
Anuj Rao <anr044@ucsd.edu>
2016-04-04 01:57:40 -0700

    Update counter.v

--- counter.v
+++ counter.v
@@ -4,13 +4,13 @@
 //
 
 module counter #(parameter width_p=16
-                                  , init_val_p=0)
+                 , init_val_p=0)
    (input clk
     , input reset_i
     , output logic [width_p-1:0] ctr_r_o
    );
 
-	always_ff @(posedge clk)
+   always_ff @(posedge clk)
      begin
        if (reset_i)
          ctr_r_o <= init_val_p;
@@ -18,4 +18,4 @@ module counter #(parameter width_p=16
          ctr_r_o <= ctr_r_o+1;
      end
 
-endmodule // counter
+endmodule
